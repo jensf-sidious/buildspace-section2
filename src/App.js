aa6eb576ca4ad47004e7cca94837089cfd42cc90
Tim Delle <tidelle@goldbeck.biz>
2022-01-24 20:24:15 +0100

    [main] Modified bio part

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        Hey I am Don Teddy 🐈.
         </div>
 
         <button className="waveButton" onClick={wave}>
